7e76f7cf4720ccb05cd16b8d4519afdfbb5f519a
Kristof Jozsa <kristof.jozsa@gmail.com>
2012-07-12 10:35:44 +0200

    so-so working example

--- src/main/scala/com/fsdev/scw/PushPage.scala
+++ src/main/scala/com/fsdev/scw/PushPage.scala
@@ -7,21 +7,23 @@ import org.apache.wicket.markup.html.WebPage
 import org.apache.wicket.markup.html.basic.Label
 import org.apache.wicket.ajax.AjaxRequestTarget
 import org.apache.wicket.atmosphere.Subscribe
+import org.apache.wicket.model.Model
+import java.util.Date
+import java.text.DateFormat
 
 /**
  * @author kjozsa
  */
 class PushPage extends WebPage with Logging {
-  val pushLabel = new Label("pushLabel")
+  val pushLabel = new Label("pushLabel", new Model)
   pushLabel.setOutputMarkupId(true)
   add(pushLabel)
 
   @Subscribe
-  def update(target: AjaxRequestTarget, event: Any) {
+  def update(target: AjaxRequestTarget, event: Date) {
     logger.debug("push update event: {}", event)
 
-    pushLabel.setDefaultModelObject(event)
+    pushLabel.setDefaultModelObject(event.getTime)
     target.add(pushLabel)
   }
-
 }
