c2a0c77218cbde73ca51fc4edaca3be1ade1ee76
Kristof Jozsa <kristof.jozsa@gmail.com>
2012-07-21 19:07:37 +0200

    form switched to ajax

--- src/main/scala/com/fsdev/scw/richmodel/FormPanel.scala
+++ src/main/scala/com/fsdev/scw/richmodel/FormPanel.scala
@@ -10,6 +10,8 @@ import com.fsdev.scw.richmodel.ModelFactory.on
 import com.fsdev.scw.Logging
 import org.apache.wicket.markup.html.form.Form
 import org.apache.wicket.markup.html.panel.Panel
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink
+import org.apache.wicket.ajax.AjaxRequestTarget
 
 /**
  * @author kjozsa
@@ -23,12 +25,15 @@ class TestModel extends Serializable {
 class FormPanel(id: String) extends Panel(id) with Logging {
   val testModel = new TestModel
 
-  val form = new Form("form") {
-    override def onSubmit() {
+  val form = new Form("form")
+  add(form)
+
+  val submit = new AjaxSubmitLink("submit") {
+    override def onSubmit(target: AjaxRequestTarget, form: Form[_]) {
       logger.info("submitted model: {}", testModel)
     }
   }
-  add(form)
+  form.add(submit)
 
   form.add(new TextField("name", on(testModel)(_.label)))
   add(new FeedbackPanel("feedback"))
